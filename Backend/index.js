b6a8296a8e8c33e9aa3822557450c78b4350fe92
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-15 13:49:52 -0400

    added more code

--- Backend/index.js
+++ Backend/index.js
@@ -3,27 +3,33 @@
 import express, {Router} from 'express';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import cors from 'cors;'
 
 
-const app = express();
+import authRoutes from './routes/authRoutes.js';
+import taskRoutes from './routes/taskRoutes.js';
+
+
 const PORT = process.env.PORT;
 
+Dotenv.config();
+const app = express();
+
+
+app.use(cors());
 app.use(express.json())
 
 app.get('/', (req, res)=>{
     res.send("Moon");
-})
-
-app.listen(3000, ()=>{
-    console.log(`listening on port: ${PORT}`)
 });
+
+
 
 await mongoose.connect('MONGO_URL= mongodb+srv://munamohamed2890:dudewheresmycode@mooncluster0.ouav8ua.mongodb.net/?retryWrites=true&w=majority&TheChillQuest=Mooncluster0')
 .then(()=>{
     console.log(`Connected to database`)
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port: ${ PORT}`)
+    app.listen(process.env.PORT, ()=>{
+        console.log(`Server is running on port: ${ process.env.PORT}`)
     });
-}).catch(()=>{
-    console.log(`connection failed`)
-});
+}).catch((error)=>
+    console.error(error));
--- Backend/model/task.js
+++ Backend/model/task.js
@@ -1,7 +1,23 @@
 import mongoose from "mongoose";
 
 const taskSchema = new mongoose.Schema({
-
-});
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+      type: String,
+      required: true  
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    category: {
+        type: String,
+        enum: ['quest', 'habit', 'daily'],
+        default: 'quest'
+    }, });
 
 export default mongoose.model("Task", taskSchema);
