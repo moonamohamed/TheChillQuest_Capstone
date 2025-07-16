80c737a05061d440a950bc7b76401672bf03da0a
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 10:57:37 -0400

    added code

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -6,4 +6,20 @@ const router = express.Router();
 router.get('/:userId', async (req, res) => {
     const tasks = await Task.find({ userId: req.params.userId});
     res.json(tasks);
-})
+});
+
+router.post('/', async(req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+    res,status(201).json(task);
+});
+
+router.patch('/:id', async (req, res) => {
+    const updated = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true});
+    res.json(updated);
+});
+
+router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(204).end(;)
+});
