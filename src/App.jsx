407ea240255fe89fee5b213a044f965534251952
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 11:16:37 -0400

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import {Routes, Route} from 'react-router-dom';
+import Navbar from './componets/Navbar';
+import Login from '.pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dasboard';
+import QuestLog from './pages/QuestLog';
 
 
 function App() {
