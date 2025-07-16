c06c4b3bc2906ae6566ac2fcc554299c940fb85c
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 10:17:28 -0400

    added more code

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -2,3 +2,12 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import user from '../models/user.js';
+
+
+const router = express.Router();
+
+router.post('/register', async (requestAnimationFrame, res) => {
+    const {username, password} = req.body;
+    const existingUser = await User.findById({username});
+    if (existingUser) return res.status(400).json({message: 'Username already exists'});
+})
