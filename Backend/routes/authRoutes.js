d2f61fbb9eef8553311bdab87643a117c2b59c16
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 10:29:02 -0400

    added code

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -9,10 +9,16 @@ const router = express.Router();
 router.post('/register', async (requestAnimationFrame, res) => {
     const {username, password} = req.body;
     const existingUser = await User.findById({username});
-    if (existingUser) return res.status(400).json({message: 'Username already exists'});
+    if(existingUser) return res.status(400).json({message: 'Username already exists'});
 
     const hash = await bcrypt.hash(password, 10);
     const user = new User({username, password: hash});
     await user.save();
     res.status(201).json({message: 'User registered'});
 });
+
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    const user = await User.findById({username});
+    if(!user) return res.status(401).json({message: 'invalid credentials'});
+})
