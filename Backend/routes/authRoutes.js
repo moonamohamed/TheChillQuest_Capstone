93019064372d01954627672716f8d8fcfc672d89
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 10:21:49 -0400

    added code

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -10,4 +10,9 @@ router.post('/register', async (requestAnimationFrame, res) => {
     const {username, password} = req.body;
     const existingUser = await User.findById({username});
     if (existingUser) return res.status(400).json({message: 'Username already exists'});
-})
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = new User({username, password: hash});
+    await user.save();
+    res.status(201).json({message: 'User registered'});
+});
