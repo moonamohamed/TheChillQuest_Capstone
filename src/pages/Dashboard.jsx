1ea1606c69e6eeb99d9ffe84d7dc89fa7e1abe04
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-07-16 14:13:28 -0400

    added code

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ export default function Dashboard() {
         if(!token) return;
         const fetchUser = async () => {
             try{
-                const res = await axios.get('http://localhost:5000/api/auth/me', {
+                const res = await axios.get('http://localhost:5173/api/auth/me', {
                     headers: {Authorization: `Bearer ${token}`},
                 });
                 setUser(res.data);
